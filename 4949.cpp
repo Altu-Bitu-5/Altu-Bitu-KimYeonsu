192ee6f185e2dbd3f2bbfe60bf0cf65c3ba2dc1c
Yeonsoo <127713796+sooooscode@users.noreply.github.com>
2023-08-29 20:45:01 +0900

    Create 4949.cpp

---
+++ 4949.cpp
@@ -0,0 +1,50 @@
+//과제2번: 4949번 균형잡힌 세상
+#include <iostream>
+#include <stack>
+//스택:LIFO
+//push():top에 원소 추가, pop(): 삭제, top(): 리턴,
+//empty():스택이 비어있으면 true, size(): 스택 사이즈 리턴
+#include <string>
+
+using namespace std;
+
+bool isBalanced(const string& expression) {
+    stack<char> s;
+
+    for (char c : expression) {
+        if (c == '(' || c == '[') {
+            s.push(c);
+        } else if (c == ')') {
+            if (s.empty() || s.top() != '(') {
+                return false;
+            }
+            s.pop();
+        } else if (c == ']') {
+            if (s.empty() || s.top() != '[') {
+                return false;
+            }
+            s.pop();
+        }
+    }
+
+    return s.empty();
+}
+
+int main() {
+    string input;
+
+    while (true) {
+        getline(cin, input);
+        if (input == ".") {
+            break;
+        }
+
+        if (isBalanced(input)) {
+            cout << "yes" << endl;
+        } else {
+            cout << "no" << endl;
+        }
+    }
+
+    return 0;
+}
