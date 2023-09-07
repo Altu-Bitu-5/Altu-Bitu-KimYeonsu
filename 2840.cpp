9d13f9b3fc4866248d8122f9af8d09c65c65403e
Yeonsoo <127713796+sooooscode@users.noreply.github.com>
2023-09-07 22:30:09 +0900

    Update 2840.cpp

--- 2840.cpp
+++ 2840.cpp
@@ -1,37 +1,71 @@
+//과제2번: 2840번 행운의 바퀴
 #include <iostream>
 #include <vector>
-#include <string>
 
 using namespace std;
 
-int main() {
-    int N, K;
-    cin >> N >> K;
+typedef pair<int, char> ic;
+const int ALPHA = 26;
 
-    string wheel = "NITAVRHO"; // 초기 바퀴 상태
-    vector<pair<int, char>> rotations; // 회전 정보를 저장할 벡터
-
-    for (int i = 0; i < K; i++) {
-        int S;
-        char stop_at;
-        cin >> S >> stop_at;
-        
-        rotations.push_back({S, stop_at});
+//출력 합수
+string printWheel(int n, int index, vector<char>&wheel){
+    string ans = "";
+    
+    //바퀴 회전과 반대로 출출력
+    for (int i = index + n; i>index; i--){
+        ans += wheel[i%n];
     }
+    return ans;
+    
+}
 
-    // 마지막 회전에서 바퀴에 적힌 알파벳을 계산
-    for (int i = K - 1; i >= 0; i--) {
-        int S = rotations[i].first;
-        char stop_at = rotations[i].second;
+
+string makeWheel(int n, int k, vector<ic>&record){
+    vector<char> wheel(n, '?'); //?로 초기화
+    vector<bool> is_available(ALPHA, false);    //알파벳 중복 체크 벡터
+    
+    int index = 0;  //화살표가 가리키는 인덱스
+    
+    for(int i=0;i<k;i++){
+        int s = record[i].first;    //변하는 횟수
+        char ch = record[i].second; //회전 후 가리키는 글자
         
-        int arrow_position = S % N; // 화살표의 최종 위치
-        int stop_index = (N - arrow_position) % N; // 회전을 멈추는 위치의 인덱스
-
-        wheel[stop_index] = stop_at;
-        N--; // 바퀴 크기 감소
+        index = (index + s) % n;    //회전 후 화살표 인인덱스
+        
+        //이미 ch로 채워짐
+        if(wheel[index] == ch){
+            continue;
+        }
+        //다른 글자로 채워짐 or 해당 글자가 이미 사용됨
+        if (wheel[index] != '?' || is_available[ch-'A']){   //A가 0이 됨
+            return "!";
+        }
+        //비어있는 경우
+        wheel[index] = ch;  //해당 칸에 적기
+        is_available[ch-'A']=true;  //사용됨
+        
     }
+    return printWheel(n,index,wheel);
+    
+}
 
-    cout << wheel << endl;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    //입력
+    int n,k;
+    cin >> n >> k;
+    
+    vector<ic> record(k);
+    for(int i=0;i<k;i++){
+        cin >> record[i].first >> record[i].second;
+    }
+    
+    //연산 & 출력
+    cout << makeWheel(n, k, record);
 
     return 0;
 }
