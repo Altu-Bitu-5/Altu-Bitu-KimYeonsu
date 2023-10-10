a8cd6ff8652e0f3bb3fe737394718c1cced8514f
Yeonsoo <127713796+sooooscode@users.noreply.github.com>
2023-10-10 14:57:19 +0900

    Update 19538.cpp

--- 08_DFS_BFS/19538.cpp
+++ 08_DFS_BFS/19538.cpp
@@ -13,16 +13,19 @@ vector<int> time;
 vector<int> neighbor;
 
 void bfs() {
-    while (!q.empty()) {
+    while (!q.empty()) 
+    {
         int nowNode = q.front();
         q.pop();
 
-        for (int next : graph[nowNode]) {
+        for (int next : graph[nowNode]) 
+        {
             // 이웃에게 난 루머를 알고있다고 전파
             neighbor[next]++;
 
             // 시간이 기록 돼 있지 않고 && 본인 노드 수 / 2 <= 본인 이웃 노드가 알고있는 루머 수
-            if (time[next] == -1 && (graph[next].size() + 1) / 2 <= neighbor[next]) {
+            if (time[next] == -1 && (graph[next].size() + 1) / 2 <= neighbor[next]) 
+            {
                 q.push(next);
                 time[next] = time[nowNode] + 1;
             }
@@ -30,15 +33,19 @@ void bfs() {
     }
 }
 
-int main() {
+int main() 
+{
+    
     cin >> n;
     graph.resize(n + 1);
     time.resize(n + 1, -1);
     neighbor.resize(n + 1, 0);
 
-    for (int i = 1; i <= n; i++) {
+    for (int i = 1; i <= n; i++) 
+    {
         int a;
-        while (true) {
+        while (true) 
+        {
             cin >> a;
             if (a == 0) break;
             graph[i].push_back(a);
@@ -47,7 +54,8 @@ int main() {
 
     cin >> m;
 
-    for (int i = 0; i < m; i++) {
+    for (int i = 0; i < m; i++) 
+    {
         int node;
         cin >> node;
         time[node] = 0;
@@ -56,7 +64,8 @@ int main() {
 
     bfs();
 
-    for (int i = 1; i <= n; i++) {
+    for (int i = 1; i <= n; i++) 
+    {
         cout << time[i];
         if (i != n) cout << " ";
     }
